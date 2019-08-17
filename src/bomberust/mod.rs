4f314bc6f417e8e21057bc380a0c3e4804d0b413
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-08-16 23:16:18 -0400

    map: make map playable by removing block if necessary

--- src/bomberust/mod.rs
+++ src/bomberust/mod.rs
@@ -87,16 +87,22 @@ pub struct Square {
     pub sq_type: SquareType
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 pub struct Player {
+    pub x: usize,
+    pub y: usize
 }
 
-pub trait Item {
-    fn walkable(&self, p: Player) -> bool;
+pub trait Walkable {
+    fn walkable(&self, p: &Player, pos: &(usize, usize)) -> bool;
 }
 
-impl Item for Bomb {
-    fn walkable(&self, _p: Player) -> bool {
+
+pub trait Item: Walkable  {
+}
+
+impl Walkable for Bomb {
+    fn walkable(&self, _p: &Player, _pos: &(usize, usize)) -> bool {
         false
     }
 }
@@ -105,13 +111,32 @@ pub struct DestructibleBox {
     
 }
 
-impl Item for DestructibleBox {
-    fn walkable(&self, _p: Player) -> bool {
+impl Walkable for DestructibleBox {
+    fn walkable(&self, _p: &Player, _pos: &(usize, usize)) -> bool {
         false
     }
 }
 
+impl Item for DestructibleBox {}
+
 type InteractiveItem = Box<dyn Item>;
+
+impl Walkable for SquareType {
+    fn walkable(&self, p: &Player, pos: &(usize, usize)) -> bool {
+        match self {
+            SquareType::Empty => true,
+            SquareType::Wall(w) => {
+                match w {
+                    Direction::North => p.x >= pos.0,
+                    Direction::South => p.x <= pos.0,
+                    Direction::West => p.y <= pos.1,
+                    Direction::East => p.y >= pos.1,
+                }
+            },
+            _ => false
+        }
+    }
+}
 
 pub struct Map {
     pub w: usize,
@@ -123,10 +148,17 @@ pub struct Map {
 }
 
 impl Map {
-    pub fn new(w: usize, h: usize) -> Map {
+    pub fn new(mut w: usize, mut h: usize) -> Map {
+        if w < 11 {
+            w = 11;
+        }
+        if h < 11 {
+            h = 11;
+        }
         let size = (w * h) as usize;
         let mut squares = Vec::with_capacity(size);
         let mut items: Vec<Option<InteractiveItem>> = Vec::with_capacity(size);
+        let mut players = Vec::new();
         let mut x = 0;
         let mut y = 0;
         let mut rng = rand::thread_rng();
@@ -153,12 +185,163 @@ impl Map {
                 y += 1;
             }
         }
-        Map {
+        // Generate players
+        for p in 0..4 {
+            let mut valid_pos = false;
+            let mut posx: usize = 0;
+            let mut posy: usize = 0;
+            let mut player = Player {
+                x: 0,
+                y: 0
+            };
+            while !valid_pos {
+                let random_x : usize = rng.gen();
+                let random_y : usize = rng.gen();
+                posx = random_x % (w / 4);
+                posy = random_y % (h / 4);
+                if p == 1 || p == 3 {
+                    posx = w - posx - 1;
+                }
+                if p == 2 || p == 3 {
+                    posy = h - posy - 1;
+                }
+                player.x = posx;
+                player.y = posy;
+                if squares[posx + posy * w].sq_type.walkable(&player, &(posx, posy)) {
+                    valid_pos = true;
+                }
+            }
+            items[posx + posy * w] = None;
+            players.push(player);
+        }
+        let mut res = Map {
             w,
             h,
             squares,
-            players: Vec::new(),
+            players,
             items
+        };
+        res.make_startable();
+        res
+    }
+
+    fn make_startable(&mut self) {
+        for p in &self.players {
+            let mut rng = rand::thread_rng();
+            let mut different_x = false;
+            let mut different_y = false;
+            let mut destroyable: Vec<(usize, usize)> = Vec::new();
+            let mut safe: Vec<(usize, usize)> = Vec::new();
+            safe.push((p.x, p.y));
+
+            let mut safe_idx = 0;
+            let mut prefer_n: bool = rng.gen();
+            let mut prefer_w: bool = rng.gen();
+            let mut check_x = true;
+            let mut inc_x: i32 = 0;
+            let mut inc_y: i32 = 0;
+            let mut direction_tested: u8 = 0;
+            while !different_x || !different_y {
+                if direction_tested == 4 {
+                    direction_tested = 0;
+                    prefer_n = rng.gen();
+                    prefer_w = rng.gen();
+                    let current = safe[safe_idx].clone();
+                    safe_idx += 1;
+                    if safe_idx >= safe.len() {
+                        if destroyable.len() == 0 {
+                            break;
+                        }
+                        let new_safe = destroyable.pop().unwrap();
+                        let linearized_pos = new_safe.0 + new_safe.1 * self.w;
+                        let walkable_item = match &self.items[linearized_pos] {
+                            Some(i) => i.walkable(p, &(new_safe)),
+                            None => true
+                        };
+                        safe.push(new_safe);
+                        if !walkable_item {
+                            self.items[linearized_pos] = None;
+                        } else {
+                            self.squares[linearized_pos].sq_type = SquareType::Empty;
+                        }
+                        if new_safe.0 != current.0 {
+                            different_x = true;
+                        } else if new_safe.1 != current.1 {
+                            different_y = true;
+                        }
+                    }
+                }
+                if check_x {
+                    if prefer_w {
+                        inc_x -= 1;
+                    } else {
+                        inc_x += 1;
+                    }
+                } else {
+                    if prefer_n {
+                        inc_y -= 1;
+                    } else {
+                        inc_y += 1;
+                    }
+                }
+                let to_test_x: i32 = safe[safe_idx].0 as i32 + inc_x;
+                let to_test_y: i32 = safe[safe_idx].1 as i32 + inc_y;
+                if to_test_x < 0 || to_test_x >= self.w as i32 {
+                    inc_x = 0;
+                    check_x = !check_x;
+                    direction_tested += 1;
+                    prefer_w = !prefer_w;
+                    continue;
+                }
+                if to_test_y < 0 || to_test_y >= self.h as i32 {
+                    inc_y = 0;
+                    check_x = !check_x;
+                    direction_tested += 1;
+                    prefer_n = !prefer_n;
+                    continue;
+                }
+                if safe.iter().find(|&&x| x == (to_test_x as usize, to_test_y as usize)) != None {
+                    if check_x {
+                        inc_x = 0;
+                        prefer_w = !prefer_w;
+                    } else {
+                        inc_y = 0;
+                        prefer_n = !prefer_n;
+                    }
+                    check_x = !check_x;
+                    direction_tested += 1;
+                    continue;
+                }
+                let linearized_pos = to_test_x as usize + to_test_y as usize * self.w;
+                let walkable_item = match &self.items[linearized_pos] {
+                    Some(i) => i.walkable(p, &(to_test_x as usize, to_test_y as usize)),
+                    None => true
+                };
+                if self.squares[linearized_pos].sq_type.walkable(p, &(to_test_x as usize, to_test_y as usize)) && walkable_item {
+                    safe.push((to_test_x as usize, to_test_y as usize));
+                    if check_x {
+                        different_x = true;
+                    } else {
+                        different_y = true;
+                    }
+                } else {
+                    if !walkable_item || self.squares[linearized_pos].sq_type != SquareType::Block {
+                        destroyable.push((to_test_x as usize, to_test_y as usize));
+                    }
+                }
+
+                if check_x {
+                    inc_x = 0;
+                    check_x = !check_x;
+                    direction_tested += 1;
+                    prefer_w = !prefer_w;
+                } else {
+                    inc_y = 0;
+                    check_x = !check_x;
+                    direction_tested += 1;
+                    prefer_n = !prefer_n;
+                }
+            }
         }
     }
 }
@@ -168,21 +351,32 @@ impl fmt::Display for Map {
         let mut map_str = String::new();
         let mut x = 0;
         for sq in &self.squares {
-            match sq.sq_type {
-                SquareType::Water => map_str.push('W'),
-                SquareType::Empty => {
-                    match self.items[x] {
-                        Some(_) => map_str.push('D'),
-                        _ => map_str.push('X')
-                    }
-                },
-                SquareType::Block => map_str.push('B'),
-                SquareType::Wall(d) => {
-                    match d {
-                        Direction::North => map_str.push('N'),
-                        Direction::South => map_str.push('S'),
-                        Direction::West  => map_str.push('W'),
-                        Direction::East  => map_str.push('E'),
+            // Test if it's a player
+            let mut is_player_here = false;
+            for p in &self.players {
+                if (p.x + p.y * self.w) == x {
+                    is_player_here = true;
+                    map_str.push('P');
+                }
+            }
+            if !is_player_here {
+                // Draw square
+                match sq.sq_type {
+                    SquareType::Water => map_str.push('W'),
+                    SquareType::Empty => {
+                        match self.items[x] {
+                            Some(_) => map_str.push('D'),
+                            _ => map_str.push('X')
+                        }
+                    },
+                    SquareType::Block => map_str.push('B'),
+                    SquareType::Wall(d) => {
+                        match d {
+                            Direction::North => map_str.push('N'),
+                            Direction::South => map_str.push('S'),
+                            Direction::West  => map_str.push('W'),
+                            Direction::East  => map_str.push('E'),
+                        }
                     }
                 }
             }
