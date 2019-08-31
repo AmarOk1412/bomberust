d2146318e3ae80e0af76f3e974d0fe9c1212ead4
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-08-31 10:54:45 -0400

    tls: listen to incoming TLS connections
    A TLS server will be the entry point for the clients. For now, add
    a dummy server to echo what the client is writing.

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,71 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **/
 
+extern crate clap;
 extern crate rand;
+extern crate tokio;
+extern crate tokio_rustls;
 
 pub mod bomberust;
 
 use bomberust::game::Game;
 
+use std::fs::File;
+use std::io::{BufReader, Write};
+use std::net::ToSocketAddrs;
+use tokio::io::{ self, AsyncRead };
+use tokio::net::TcpListener;
+use tokio::prelude::{Future, Stream};
+use tokio_rustls::{
+    TlsAcceptor,
+    rustls::{
+        Certificate, NoClientAuth, PrivateKey, ServerConfig,
+        internal::pemfile::{ certs, rsa_private_keys }
+    },
+};
+use std::sync::Arc;
+use std::thread;
+
+fn load_certs(path: &str) -> Vec<Certificate> {
+    certs(&mut BufReader::new(File::open(path).unwrap())).unwrap()
+}
+
+fn load_keys(path: &str) -> Vec<PrivateKey> {
+    rsa_private_keys(&mut BufReader::new(File::open(path).unwrap())).unwrap()
+}
+
 fn main() {
+    let server_thread = thread::spawn(move || {
+        let addr = "0.0.0.0:2542".to_socket_addrs().unwrap().next().unwrap();
+
+        let mut config = ServerConfig::new(NoClientAuth::new());
+        config.set_single_cert(load_certs("./keys/ca/rsa/end.fullchain"), load_keys("./keys/ca/rsa/end.rsa").remove(0))
+            .expect("invalid key or certificate");
+        let config = TlsAcceptor::from(Arc::new(config));
+
+        let socket = TcpListener::bind(&addr).unwrap();
+        let done = socket.incoming()
+            .for_each(move |stream| {
+                let addr = stream.peer_addr().ok();
+            let done = config.accept(stream)
+                .and_then(|stream| {
+                    let (reader, writer) = stream.split();
+                    io::copy(reader, writer)
+                    //io::write_all(stream, &b"HELLO FROM SERVER"[..])
+                })
+                .map(move |(n, ..)| println!("Echo: {} - {:?}", n, addr))
+                //.and_then(|(stream, _)| io::flush(stream))
+                //.map(move |_| println!("Accept: {:?}", addr))
+                .map_err(move |err| println!("Error: {:?} - {:?}", err, addr));
+                tokio::spawn(done);
+
+                Ok(())
+            });
+
+        tokio::run(done.map_err(drop));
+    });
+
     let mut g = Game::new();
     g.start();
+    let _ = server_thread.join();
 }
